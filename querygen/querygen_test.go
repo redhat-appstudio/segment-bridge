328708c2de72b0763288a2723eeba7e95931a0ca
Barak Korren <bkorren@redhat.com>
2023-07-13 11:16:52 +0300

    Added a query for build PipelineRun creaiton
    Just an internal function in querygen for now, still need to make it
    print it.

--- querygen/querygen_test.go
+++ querygen/querygen_test.go
@@ -16,6 +16,8 @@ func TestGenDedupEval(t *testing.T) {
 		"objectRef.name",
 		"verb",
 		"requestReceivedTimestamp",
+		"responseObject.metadata.labels.appstudio.openshift.io/application",
+		"responseObject.metadata.labels.appstudio.openshift.io/component",
 	}
 	expected := `eval ` +
 		`auditID=mvindex('auditID', 0),` +
@@ -27,7 +29,11 @@ func TestGenDedupEval(t *testing.T) {
 		`objectRef.apiVersion=mvindex('objectRef.apiVersion', 0),` +
 		`objectRef.name=mvindex('objectRef.name', 0),` +
 		`verb=mvindex('verb', 0),` +
-		`requestReceivedTimestamp=mvindex('requestReceivedTimestamp', 0)`
+		`requestReceivedTimestamp=mvindex('requestReceivedTimestamp', 0),` +
+		"responseObject.metadata.labels.appstudio.openshift.io/application" +
+		"=mvindex('responseObject.metadata.labels.appstudio.openshift.io/application', 0)," +
+		"responseObject.metadata.labels.appstudio.openshift.io/component" +
+		"=mvindex('responseObject.metadata.labels.appstudio.openshift.io/component', 0)" 
 	out := GenDedupEval(fields)
 	if out != expected {
 		t.Errorf("GenDedupEval() returns:\n  %q, expected\n  %q", out, expected)
@@ -53,6 +59,11 @@ func TestGenApplicationQuery(t *testing.T) {
 		"kind":       "objectRef.resource",
 		"name":       "objectRef.name",
 	}
+	track_fields := TrackFieldSpec{
+		with_userid: true,
+		with_ev_verb: true,
+		with_ev_subject: true,
+	}
 	expected := `search ` +
 		`index="some_index" ` +
 		`log_type=audit ` +
@@ -63,7 +74,7 @@ func TestGenApplicationQuery(t *testing.T) {
 		`("impersonatedUser.username"="*" OR (user.username="*" AND NOT user.username="system:*")) ` +
 		`(verb!=create OR "responseObject.metadata.resourceVersion"="*")` +
 		`|` + GenDedupEval(fields) +
-		`|` + GenTrackFields(TrackFieldSpec{true, true, true}, json_properties)
+		`|` + GenTrackFields(track_fields, json_properties)
 	out := GenApplicationQuery("some_index")
 	if out != expected {
 		t.Errorf("GenApplicationQuery() = %v, want %v", out, expected)
@@ -140,6 +151,26 @@ func TestGenTrackFields(t *testing.T) {
 				`properties=` + GenPropertiesJSONExpr(json_properties) +
 				`|` + IncludeFieldsCmd + `|` + ExcludeFieldsCmd,
 		},
+		{
+			name: "with namespace, verb, subject & app props",
+			args: args{
+				spec: TrackFieldSpec{
+					with_namespace:  true,
+					with_ev_verb:    true,
+					with_ev_subject: true,
+				},
+				properties_map: json_properties,
+			},
+			want: `eval ` +
+				`messageId=auditID,` +
+				`timestamp=requestReceivedTimestamp,` +
+				`type="track",` +
+				`namespace='objectRef.namespace',` +
+				`event_verb=verb,` +
+				`event_subject='objectRef.resource',` +
+				`properties=` + GenPropertiesJSONExpr(json_properties) +
+				`|` + IncludeFieldsCmd + `|` + ExcludeFieldsCmd,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -147,5 +178,45 @@ func TestGenTrackFields(t *testing.T) {
 				t.Errorf("GenTrackFields() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestGenPipelineRunQuery(t *testing.T) {
+	fields := []string{
+		"auditID",
+		"objectRef.resource",
+		"objectRef.namespace",
+		"objectRef.apiGroup",
+		"objectRef.apiVersion",
+		"verb",
+		"requestReceivedTimestamp",
+		"responseObject.metadata.labels.appstudio.openshift.io/application",
+		"responseObject.metadata.labels.appstudio.openshift.io/component",
+	}
+	json_properties := map[string]string{
+		"apiGroup":    "objectRef.apiGroup",
+		"apiVersion":  "objectRef.apiVersion",
+		"kind":        "objectRef.resource",
+		"application": "responseObject.metadata.labels.appstudio.openshift.io/application",
+		"component": "responseObject.metadata.labels.appstudio.openshift.io/component",
+	}
+	track_fields := TrackFieldSpec{
+		with_namespace: true,
+		with_ev_verb: true,
+		with_ev_subject: true,
+	}
+	expected := `search ` +
+		`index="some_index" ` +
+		`log_type=audit ` +
+		`verb=create ` +
+		`"responseStatus.code" IN (200, 201) ` +
+		`"objectRef.apiGroup"="tekton.dev" ` +
+		`"objectRef.resource"="pipelineruns" ` +
+		`"responseObject.metadata.labels.pipelines.appstudio.openshift.io/type"=build` +
+		`"responseObject.metadata.resourceVersion"="*"` +
+		`|` + GenDedupEval(fields) +
+		`|` + GenTrackFields(track_fields, json_properties)
+	if out := GenPipelineRunQuery("some_index"); out != expected {
+		t.Errorf("GenPipelineRunQuery() = %v, want %v", out, expected)
 	}
 }
