4728c4d72cd649d118e7b1a520c7fc64bd73b5d2
Omer <oamsalem@redhat.com>
2023-11-02 17:57:24 +0200

    chore(RHTAPWATCH-505): ws-map-maker fail on connection errors
    The ws-map-maker script ignored cases where
    there was a connection failure. this change
    aim to catch those failures and report them
    for better visibility of the error.
    
    Signed-off-by: Omer <oamsalem@redhat.com>

--- scripts/ws-map-maker-job.sh
+++ scripts/ws-map-maker-job.sh
@@ -11,6 +11,7 @@
 #
 set -o pipefail -o errexit -o nounset -o xtrace
 
-oc create configmap --dry-run=client -o yaml ws-map \
-  --from-file=ws-map.json=<(KUBECONFIG="$KUBECONFIG_SRC" get-workspace-map.sh) \
-| oc apply -f -
+if ! oc create configmap --dry-run=client -o yaml ws-map \
+  --from-file=ws-map.json=<(KUBECONFIG="$KUBECONFIG_SRC" get-workspace-map.sh) | oc apply -f -; then
+  echo "Error: Encountered an issue while applying the ConfigMap."
+fi
